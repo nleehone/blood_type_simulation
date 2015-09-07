a3c4cbb4e41ebfec40cd38bf83e6fd8f124c8ddf
Nicholas Lee-Hone <nicholas.leehone@gmail.com>
2015-09-07 16:39:30 -0700

    Attempt failed

--- main.py
+++ main.py
@@ -5,30 +5,59 @@ import numpy.random as random
 
 import matplotlib.pyplot as plt
 
-# Blood types are represented by strings of length 2
+"""The basic types are represented by:
+'o' : 0
+'a' : 1
+'b' : 4
+The full blood types are therefore represented by:
+'oo': 0
+'ao': 1
+'oa': 1
+'aa': 2
+'bo': 4
+'ob': 4
+'bb': 8
+'ab': 5
+"""
+# The fractions represent how many people start with a certain blood type
+# Ordered: ['o', 'a', 'b', 'ab']
 fractions = [0.25, 0.25, 0.25, 0.25]
 size = 10000
 num_generations = 100
+
+blood_types_to_base = {
+    0: [0, 0],
+    1: [0, 1],
+    2: [1, 1],
+    4: [0, 4],
+    8: [4, 4],
+    5: [4, 1]
+}
 
 
 def create_initial_blood_types(size, fractions):
     blood_types = []
     for index in range(size):
         if index < size*fractions[0]:
-            bt = 'oo'
+            bt = 0
+            bt = 5
         elif index < size*(fractions[1] + fractions[0]):
-            bt = random.choice(['aa', 'ao', 'oa'])
+            # Need to repeat the '1' so that we can have 'ao' and 'oa'
+            bt = random.choice([1, 1, 2])
         elif index < size*(fractions[2] + fractions[1] + fractions[0]):
-            bt = random.choice(['bb', 'bo', 'ob'])
+            # Need to repeat the '4' so that we can have 'bo' and 'ob'
+            bt = random.choice([4, 4, 8])
         else:
-            bt = 'ab'
-        blood_types.append([bt[0], bt[1]])
+            bt = 5
+        blood_types.append(bt)
     return np.array(blood_types)
 
 
 def create_new_blood_type(blood_type_1, blood_type_2):
     # Choose either the first or second part of the blood type
-    return blood_type_1[random.randint(0, 1)] + blood_type_2[random.randint(0, 1)]
+    base_1 = blood_types_to_base[blood_type_1]
+    base_2 = blood_types_to_base[blood_type_2]
+    return base_1[random.randint(0, 2)] + base_2[random.randint(0, 2)]
 
 
 def next_generation(blood_types_1, blood_types_2):
@@ -38,9 +67,10 @@ def next_generation(blood_types_1, blood_types_2):
     rbt2 = random.randint(0, size, size=2*size)
     r1 = random.randint(2, size=2*size)
     r2 = random.randint(2, size=2*size)
-    nbt_1 = blood_types_1[rbt1, r1]
-    nbt_2 = blood_types_2[rbt2, r2]
-    nbt = np.transpose([nbt_1, nbt_2])
+    bt_1 = blood_types_1[rbt1]
+    bt_2 = blood_types_2[rbt2]
+    nbt = np.array([create_new_blood_type(bt_1[i], bt_2[i]) for i in range(size*2)])
+    #nbt = np.transpose([nbt_1, nbt_2])
     new_blood_types_1 = nbt[:size]
     new_blood_types_2 = nbt[size:]
     return new_blood_types_1, new_blood_types_2, generate_results(new_blood_types_1, new_blood_types_2)
@@ -50,17 +80,16 @@ def next_generation(blood_types_1, blood_types_2):
 def generate_results(blood_types_1, blood_types_2):
     """Figure out how many of each blood type there is"""
     ab = a = b = o = 0
-    for blood_type in np.vstack([blood_types_1, blood_types_2]):
-        blood_type = blood_type[0] + blood_type[1]
-        if blood_type in ['ab']:
+    for blood_type in np.append(blood_types_1, blood_types_2):
+        if blood_type == 5:
             ab += 1
-        elif blood_type in ['aa', 'ao', 'oa']:
+        elif blood_type in [1, 2]:
             a += 1
-        elif blood_type in ['bb', 'bo', 'ob']:
+        elif blood_type in [4, 8]:
             b += 1
         else:
             o += 1
-    size = len(blood_types_1) + len(blood_types_2)
+    size = 2*len(blood_types_1)
     return {'ab': ab/size, 'a': a/size, 'b': b/size, 'o': o/size}
 
 
